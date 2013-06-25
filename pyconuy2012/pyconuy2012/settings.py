f44e6f3663842e7daf290fc92b4556fe345dfc21
Sebastián Nogara <snogaraleal@gmail.com>
2013-06-25 13:14:59 -0300

    Fix 2012 site links.

--- pyconuy2012/pyconuy2012/settings.py
+++ pyconuy2012/pyconuy2012/settings.py
@@ -63,12 +63,15 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'cms.middleware.multilingual.MultilingualURLMiddleware',
+    #'cms.middleware.multilingual.MultilingualURLMiddleware',
+    'main.middleware.multilingual.MultilingualURLMiddleware',
     'cms.middleware.page.CurrentPageMiddleware',
     'cms.middleware.user.CurrentUserMiddleware',
     'cms.middleware.toolbar.ToolbarMiddleware',
     'cmsplugin_blog.middleware.MultilingualBlogEntriesMiddleware',
 )
+
+MULTILINGUAL_URL_PREPEND = '/2012/'
 
 ROOT_URLCONF = 'pyconuy2012.urls'
 WSGI_APPLICATION = 'pyconuy2012.wsgi.application'
