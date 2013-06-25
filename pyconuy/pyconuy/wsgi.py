b10581093bf1914cc63335b4306bc65bf30c565d
Pablo Ricco <pricco@gmail.com>
2013-06-25 15:33:46 -0300

    remove pyconuy 2012

--- pyconuy/pyconuy/wsgi.py
+++ pyconuy/pyconuy/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for pyconuy2012 project.
+WSGI config for pyconuy project.
 
 This module contains the WSGI application used by Django's development server
 and any production WSGI deployments. It should expose a module-level variable
